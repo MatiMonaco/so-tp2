b6e53af2c6cbf4720808b2c3d904b9bc7ca2d0cd
Matias Monaco <matimonaco@outlook.com>
2019-11-08 17:43:08 -0300

    termine kbdriver creo

--- Kernel/include/kbDriver.h
+++ Kernel/include/kbDriver.h
@@ -1,3 +1,53 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
 int kbFlag();
-int getKey();
-int printKey();
+int getKeyScancode();
+void keyboardHandler();
+char getKeyASCII();
+
+#define BUFFER_SIZE 256
+
+#define LEFT_SHIFT_PRESS 42
+#define RIGHT_SHIFT_PRESS 54
+#define CAPSLOCK_PRESS 58
+#define LEFT_SHIFT_RELEASE 170
+#define RIGHT_SHIFT_RELEASE 182
+
+#define CTRL 0
+#define ALT 0
+#define UP 11
+#define RIGHT 0
+#define LEFT 0
+#define DOWN 9
+#define ESC 0
+#define BACKSPACE '\b'
+#define TAB 0
+#define ENTER '\n'
+#define LEFT_SHIFT 0
+#define RIGHT_SHIFT 0
+#define LEFT_ALT 0
+#define SPACE ' '
+#define CAPSLOCK 0
+#define F1 0
+#define F2 0
+#define F3 0
+#define F4 0
+#define F5 0
+#define F6 0
+#define F7 0
+#define F8 0
+#define F9 0
+#define F10 0
+#define F11 0
+#define F12 0
+#define NUM_LOCK 0
+#define SCROLL_LOCK 0
+#define INSERT 0
+#define SUPR 0
+#define HOME 0
+#define END 0
+#define REPAG 0
+#define AVPAG 0
+
+#endif
--- Kernel/kbDriver.c
+++ Kernel/kbDriver.c
@@ -1,49 +1,92 @@
 #include <kbDriver.h>
-#include <naiveConsole.h>
-#define BUFFER_SIZE 100
-char buffer[BUFFER_SIZE] = {};
-static pos = 0;
-static unsigned char keyboard[128] =
-{
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
-  '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
-  '-',
-    0,	/* Left Arrow */
-    0,
-    0,	/* Right Arrow */
-  '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
-    0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
-};
-//adsssssssssssssssssssssssssssss
-int getKey(){
-    return kbFlag();
+#include <stdint.h>
+
+static isPressed(uint8_t scanCode);
+static addKeyToBuffer(char ascii);
+
+
+char buffer[BUFFER_SIZE] = {0};
+
+uint8_t readIndex = 0;
+uint8_t writeIndex = 0;
+uint8_t SHIFT_ON = 0;
+uint7_t CAPSLOCK_ON = 0;
+
+
+
+static unsigned char keys[] = { 0, ESC, '1', '2', '3', '4', '5', 
+'6', '7', '8', '9', '0', '-', '=', BACKSPACE,TAB, 'q', 'w','e', 'r', 't',
+'y', 'u', 'i', 'o', 'p', '[', ']', ENTER, 0,'a', 's', 'd', 'f', 'g', 'h',
+'j', 'k', 'l', ';', '\'', 0, LEFT_SHIFT,'\\','z', 'x', 'c', 'v', 'b', 'n', 'm', ',',
+'.', '/', RIGHT_SHIFT, '*', LEFT_ALT,SPACE, CAPSLOCK,F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, 
+NUM_LOCK, SCROLL_LOCK,HOME, UP, REPAG,'-', LEFT, 0, RIGHT, '+', END, DOWN,AVPAG, INSERT, 
+SUPR, 0, 0, 0, F11, F12 };
+
+
+static unsigned char keysWithShift[] = { 0, ESC, '!', '@', '#', '$', '%','^', '&', '*',
+ '(', ')', '_', '+', BACKSPACE,TAB,'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+ 'O', 'P', '{', '}', ENTER, 0,'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':','"',
+ '|', LEFT_SHIFT, '>','Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 0, 0,LEFT_ALT,SPACE, 
+ CAPSLOCK,F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, NUM_LOCK,SCROLL_LOCK,HOME, UP, REPAG, '-', LEFT,
+ 0, RIGHT, '+', END, DOWN,AVPAG, INSERT, SUPR, 0, 0, '>', F11, F12 };
+
+void keyboardHandler(){
+	
+	uint8_t scancode = getKey();
+	char ascii;
+	if(isPressed(scancode)){
+		switch(scancode){
+			case LEFT_SHIFT_PRESS:
+			case RIGHT_SHIFT_PRESS:
+				SHIFT_ON =  1;
+				break;
+			case CAPSLOCK_PRESS:
+				CAPSLOCK_ON = !CAPSLOCK_ON;
+				break;
+			default:
+				if(SHIFT_ON){
+					ascii = keysWithShift[scanCode];
+			
+				} else {
+					ascii = keys[scanCode];
+				}
+				break;
+				
+		}
+	}else if (scanCode == LEFT_SHIFT_RELEASE || scanCode == RIGHT_SHIFT_RELEASE){
+                SHIFT_ON = 0;
+				return;
+	}
+        addKeyToBuffer(ascii); 
+}
+	static addKeyToBuffer(char ascii){
+		buffer[writeIndex++] = ascii;
+		if(writeIndex > BUFFER_SIZE){
+			writeIndex = 0;
+		}
+		if(writeIndex == readIndex){
+			readIndex++;
+		}
+	}
+
+	char getKeyASCII(){
+		char ascii = 0;
+		if(readIndex != writeIndex){
+			ascii = buffer[readIndex++];
+		}
+		return ascii;
+
+	}
+
+	
+	static isPressed(uint8_t scanCode){
+		return !(scancode & 0x80h);
+	}
+	
+	int getKeyScancode(){
+	   return kbFlag();
+	}
+
+
 }
 
