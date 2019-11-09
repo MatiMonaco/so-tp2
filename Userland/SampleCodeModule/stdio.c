2801bb8c698ba170fd42684986c66b93ca3bd34e
Matias Monaco <matimonaco@outlook.com>
2019-11-08 23:03:18 -0300

    asd

--- Userland/SampleCodeModule/stdio.c
+++ Userland/SampleCodeModule/stdio.c
@@ -2,16 +2,16 @@
 #include <string.h>
 #include <callSyscall.h>
 void putchar(char c){
-	callSyscall(WRITE,(uint64_t)1,(const char*)&c,(uint64_t)1,0,0,0);
+	callSyscall(WRITE,1,&c,1,0,0,0);
 }
 char getchar(){
 	char c;
-	callSyscall(READ,(char *)&c,(uint64_t)1,0,0,0,0);
+	callSyscall(READ,&c,1,0,0,0,0);
 	return c;
 }
 
 void printf(const char* buffer){
-	callSyscall(WRITE,(uint64_t)1,(const char*)buffer,(uint64_t)strlen(buffer),0,0,0);
+	callSyscall(WRITE,1,buffer,strlen(buffer),0,0,0);
 }
 
 void scanf(char*buffer){
