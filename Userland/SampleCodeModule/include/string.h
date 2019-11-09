5dfb01a1ad2aff08559016ae839bf4cd55aa7a3b
Matias Monaco <matimonaco@outlook.com>
2019-11-08 22:47:41 -0300

    asd

--- Userland/SampleCodeModule/include/string.h
+++ Userland/SampleCodeModule/include/string.h
@@ -1,6 +1,6 @@
 #ifndef STRING_H
 #define STRING_H
-
+#include <stdint.h>
 uint64_t strlen(const char * s);
 uint64_t strcmp(const char * s1,const char * s2);
 
--- Userland/SampleCodeModule/string.c
+++ Userland/SampleCodeModule/string.c
@@ -1,5 +1,5 @@
 #include <string.h>
-#include <stdint.h>
+
 uint64_t strlen(const char * s){
 	uint64_t size = 0;
 	while(*s){
@@ -12,8 +12,8 @@ uint64_t strcmp(const char * s1,const char * s2){
 	while (*s1 && *s2) {
 		if (*s1 > *s2) return 1;
 		if (*s1 < *s2) return -1;
-		a++;
-		b++;
+		s1++;
+		s2++;
 	}
 	if (*s1) return 1;
 	if (*s2)	return -1;
