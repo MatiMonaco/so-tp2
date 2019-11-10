0758fc00f006a1086834eeed810a1101282c3433
scottlin19 <scott.lin099@gmail.com>
2019-11-09 21:57:58 -0300

    few

--- Kernel/screenDriver.c
+++ Kernel/screenDriver.c
@@ -112,7 +112,7 @@ void reverse(char str[], int length)
 char* intToBase(int num, char* str, int base) 
 { 
     int i = 0; 
-    bool isNegative = false; 
+    int isNegative = 0; 
   
     /* Handle 0 explicitely, otherwise empty string is printed for 0 */
     if (num == 0) 
@@ -126,7 +126,7 @@ char* intToBase(int num, char* str, int base)
     // base 10. Otherwise numbers are considered unsigned. 
     if (num < 0 && base == 10) 
     { 
-        isNegative = true; 
+        isNegative = 1; 
         num = -num; 
     } 
   
