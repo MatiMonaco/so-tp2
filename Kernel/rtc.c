6f1b571117ffaa63f8195030e9f25e5e83865515
scottlin19 <scott.lin099@gmail.com>
2019-11-10 14:55:37 -0300

    f

--- Kernel/rtc.c
+++ Kernel/rtc.c
@@ -1,25 +1,30 @@
 #include <stdint.h>
 #include <rtc.h>
 #include <screenDriver.h>
-
+#include <libc.h>
 
 void displayDateTime(){
 	getCurrDate();
 	drawString(" - ");
 	getCurrTime();
 }
+
 void getCurrTime(){
-	uint64_t hs = getCurrHs();
+	char hs[3];
+	intToBase(getCurrHs(),hs,10);
 	uint64_t mins = getCurrMins();
 	uint64_t secs = getCurrSecs();
-	drawDec(hs);
+	drawString(hs);
+	/*
 	drawChar(':',0xFFFFFF,0x000000);	
 	drawDec(mins);
 	drawChar(':',0xFFFFFF,0x000000);	
 	drawDec(secs);	
+	*/
 }
 
 void getCurrDate(){
+	/*
 	uint64_t day = getCurrDay();
 	uint64_t month = getCurrMonth();
 	uint64_t year = getCurrYear();
@@ -28,4 +33,5 @@ void getCurrDate(){
 	drawDec(month);
 	drawChar('/',0xFFFFFF,0x000000);	
 	drawDec(year);	
+	*/
 }
