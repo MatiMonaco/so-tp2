17230ab15c14dc526e363087beb0d7bb721e8726
mciccioli <51211139+mciccioli@users.noreply.github.com>
2019-11-09 21:48:46 -0300

    Update screenDriver.c

--- Kernel/screenDriver.c
+++ Kernel/screenDriver.c
@@ -75,3 +75,84 @@ void scrollDown(){
 	int size = getScreenWidth() * (getScreenHeight() - 1) * getScreenBPP();
 	memcpy(start,secondLine,size);
 }
+
+
+void drawDec(uint64_t value){
+	drawBase(value, 10);
+}
+
+void drawHexa(uint64_t value){
+	drawBase(value, 16);
+}
+
+void drawBin(uint64_t value){
+	drawBase(value, 2);
+}
+
+void drawBase(uint64_t value, uint32_t base){
+	static char buffer[64] = { '0' };
+    intToBase(value, buffer, base);
+    drawString(buffer);
+}
+
+
+void reverse(char str[], int length) 
+{ 
+    int start = 0; 
+    int end = length -1; 
+    while (start < end) 
+    { 
+        swap(*(str+start), *(str+end)); 
+        start++; 
+        end--; 
+    } 
+} 
+
+
+char* intToBase(int num, char* str, int base) 
+{ 
+    int i = 0; 
+    bool isNegative = false; 
+  
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0) 
+    { 
+        str[i++] = '0'; 
+        str[i] = '\0'; 
+        return str; 
+    } 
+  
+    // In standard itoa(), negative numbers are handled only with  
+    // base 10. Otherwise numbers are considered unsigned. 
+    if (num < 0 && base == 10) 
+    { 
+        isNegative = true; 
+        num = -num; 
+    } 
+  
+    // Process individual digits 
+    while (num != 0) 
+    { 
+        int rem = num % base; 
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0'; 
+        num = num/base; 
+    } 
+  
+    // If number is negative, append '-' 
+    if (isNegative) 
+        str[i++] = '-'; 
+  
+    str[i] = '\0'; // Append string terminator 
+  
+    // Reverse the string 
+    reverse(str, i); 
+  
+    return str; 
+} 
+
+
+
+
+
+
+
