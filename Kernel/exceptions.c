7dcf33d9652228fa4a9fb9fb0c51c06cef0eac28
mciccioli <51211139+mciccioli@users.noreply.github.com>
2019-11-09 22:52:49 -0300

    Update exceptions.c

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,6 +1,24 @@
 
 #define ZERO_EXCEPTION_ID 0
+#define INVALID_CODE_ID 6
 
+void exceptionDispatcher(int exception_id){
+switch(exception_id)
+	{
+		case ZERO_EXCEPTION_ID: 
+		    drawString("ZERO DIVISION EXCEPTION");
+                    break;
+	        case INVALID_CODE_ID: 
+	            drawString("INVALID OPERATION EXCEPTION");
+		    break;
+	}
+	printRegisters();
+}
+
+
+
+
+/*
 static void zero_division();
 
 void exceptionDispatcher(int exception) {
@@ -11,3 +29,5 @@ void exceptionDispatcher(int exception) {
 static void zero_division() {
 	// Handler para manejar excepcíon
 }
+
+*/
