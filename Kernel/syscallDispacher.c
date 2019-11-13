228df8b6d388b9b1bdbc9b81cc4b46815118ab59
mciccioli <51211139+mciccioli@users.noreply.github.com>
2019-11-13 08:36:41 -0300

    Update syscallDispacher.c

--- Kernel/syscallDispacher.c
+++ Kernel/syscallDispacher.c
@@ -13,7 +13,7 @@
 
 typedef uint64_t(*SystemCall)();
 
-static SystemCall syscall_array[] = {0,(SystemCall)sys_getWidth,(SystemCall)sys_getHeight,(SystemCall)sys_read,(SystemCall)sys_write,0,(SystemCall)sys_drawRect,(SystemCall) sys_sleep};
+static SystemCall syscall_array[] = {0,(SystemCall)sys_getWidth,(SystemCall)sys_getHeight,(SystemCall)sys_read,(SystemCall)sys_write,(SystemCall)sys_speaker,(SystemCall)sys_drawRect,(SystemCall) sys_sleep};
 
 uint64_t syscallDispatcher(uint64_t id, void* param1, void* param2, void* param3, void* param4, void* param5, void* param6){
    return syscall_array[id](param1,param2,param3,param4,param5,param6);
