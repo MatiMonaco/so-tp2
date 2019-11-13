67a06beabfd9f64cf3ecf949864a74dfb6c7cbde
mciccioli <51211139+mciccioli@users.noreply.github.com>
2019-11-13 09:16:46 -0300

    Update libasm.asm

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -1,4 +1,7 @@
 GLOBAL cpuVendor
+GLOBAL beepasm
+GLOBAL unbeepasm
+
 section .text
 
 cpuVendor:
@@ -25,32 +28,27 @@ cpuVendor:
 	pop rbp
 	ret
 
-_beep_start:
-	push rbp
-	mov rbp, rsp
+beepasm:
+    push rbp
+    mov rbp, rsp
 
-	mov al, 0xB6
-	out 43h,al
+    mov rax,0;    seteo el registro de rax todo en 0.
+    mov al, 182; preparo al speaker para realizar el beep.
+    out 0x43, al
+    mov ax, 9121; cargo la frecuencia.
+    out 0x42, al; envio la parte baja del byte
+    mov al, ah
+    out 0x42, al; envio la parte alta
+    in al, 0x61
+    or al, 0x03; prendo los bits para emitir el beep.
+    out 61h, al
 
-	mov rbx, rdi
-	mov rax, 0
-	mov ax, bx
+    mov rsp, rbp
+    pop rbp
+    ret
 
-	;mov ax, 1193 ;1193180 / nFrequence;
-	out 42h,al
-	mov al,ah
-	out 42h,al
-
-	in al, 61h 
- 	or al, 03h
-	out 61h,al
-
-	mov rsp, rbp
-	pop rbp
-	ret
-
-_beep_stop:
-  	in al, 61h
-	and al, 0xFC
-	out 61h, al
-  	ret
+unbeepasm:
+    in al, 0x61
+    and al, 0xFC; apago los bits para apagar el beep.
+    out 0x61, al
+    ret
