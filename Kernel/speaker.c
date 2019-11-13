36a70ad7b8dffe0fd3333066939abc70e3bb26c2
mciccioli <51211139+mciccioli@users.noreply.github.com>
2019-11-13 09:03:34 -0300

    Update speaker.c

--- Kernel/speaker.c
+++ Kernel/speaker.c
@@ -1,16 +1,16 @@
-#include <speaker.h>
 #include <stdint.h>
+#include "include/timer.h"
+#include "include/speaker.h"
+#include "asm/libasm.h"
 
-void speaker()
-{
-  beep();
+
+void beep(uint32_t nFrequence, unsigned char duration) {
+  timer_removeFunction(&nosound);
+  timer_appendFunction(&nosound, duration);
+  _beep_start(1193180 / nFrequence);
 }
 
-void beep()
-{
- beepon();
-  for (int i = 0; i < 1000000; i++) {
-    ;
-  }
-  beepoff();
+void nosound() {
+  timer_removeFunction(&nosound);
+  _beep_stop();
 }
