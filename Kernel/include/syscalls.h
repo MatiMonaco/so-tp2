b3578047f93bf39bd190ccc0e3137bd7f70b063f
scottlin19 <scott.lin099@gmail.com>
2019-11-23 21:27:13 -0300

    ewhilge

--- Kernel/include/syscalls.h
+++ Kernel/include/syscalls.h
@@ -7,7 +7,7 @@ uint64_t sys_drawRect(uint64_t x,uint64_t y,uint64_t width,uint64_t height,uint6
 uint64_t sys_getHeight();
 uint64_t sys_getWidth();
 uint64_t sys_sleep(int ticks);
-void sys_beep(uint64_t timer);
+uint64_t sys_beep(uint64_t timer);
 uint64_t sys_drawCircle(uint64_t x,uint64_t y,uint64_t radius,uint64_t color);
 uint64_t sys_time();
 uint64_t sys_clear();
