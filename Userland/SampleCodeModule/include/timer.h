39e1f6c05c52aabbdf924f40d34ec8421179b8c1
mciccioli <51211139+mciccioli@users.noreply.github.com>
2019-11-24 03:12:23 -0300

    Update timer.h

--- Userland/SampleCodeModule/include/timer.h
+++ Userland/SampleCodeModule/include/timer.h
@@ -2,6 +2,13 @@
 uint64_t getSeconds();
 uint64_t sleep(uint64_t ticks);
 
+#define SECONDS_SELECTOR 0
+#define MINUTES_SELECTOR 1
+#define HOURS_SELECTOR 2
+#define DAY_SELECTOR 3
+#define MONTH_SELECTOR 4
+#define YEAR_SELECTOR 5
+
 
 typedef struct{
   int seconds;
@@ -11,3 +18,6 @@ typedef struct{
   int month;
   int year;
 }tm;
+
+void print_time();
+tm get_time();
